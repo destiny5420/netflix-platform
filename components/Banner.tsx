d12a6e8c715b7e28ee227c0f260505093815417d
paperhsiao(M1 Pro) <destiny5420@gmail.com>
2022-08-10 13:06:50 +0800

    feat: 在 Home page 使用 getServerSideProps 在每次 request page 時，都可以拿到新的頁面資料

---
+++ components/Banner.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import Image from 'next/image'
+import { Movie } from '../typings'
+import { baseUrl } from '../constants/movie'
+
+interface Props {
+  netflixOriginals: Movie[]
+}
+
+function Banner({ netflixOriginals }: Props) {
+  const [movie, setMovie] = useState<Movie | null>(null)
+
+  useEffect(() => {
+    setMovie(netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)])
+  }, [netflixOriginals])
+
+  return (
+    <div>
+      <div className="absolute top-0 left-0 w-screen h-[95vh]">
+        <Image layout="fill" src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path} `} />
+      </div>
+    </div>
+  )
+}
+
+export default Banner
